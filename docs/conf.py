6fb83e0d55a2adb1993c7f52fb07f93f8b25f93f
BJ Fulton <bjfultn@gmail.com>
2021-02-24 12:00:37 -0800

    trying to fix doc imports

--- docs/conf.py
+++ docs/conf.py
@@ -12,8 +12,8 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('.'))
-from pyneid import neid
+sys.path.insert(0, os.path.abspath('..'))
+import pyneid
 
 # -- Project information -----------------------------------------------------
 
