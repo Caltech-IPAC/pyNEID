5c14f3f409a79747977f4aca2f08f50c1ec7d268
BJ Fulton <bjfultn@gmail.com>
2021-03-22 09:18:36 -0700

    increment version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open ("README.md", "r") as fh:
     
 setup(
     name="pyneid",
-    version="0.1.0",
+    version="1.0.1",
     author="Mihseh Kong",
     description="NEID archive access client", 
     long_description = long_description,
